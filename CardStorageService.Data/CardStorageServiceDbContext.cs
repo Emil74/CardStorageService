b232eef2c4e20a54fa51ebd36830bdf69620f639
Emil74 <emil_nagiyev_98@mail.ru>
2023-02-23 18:02:21 +0300

    CardStorageService2

--- CardStorageService.Data/CardStorageServiceDbContext.cs
+++ CardStorageService.Data/CardStorageServiceDbContext.cs
@@ -11,6 +11,9 @@ namespace CardStorageService.Data
     {
         public virtual DbSet<Client> Clients { get; set; }
         public virtual DbSet<Card> Cards { get; set; }
+        public DbSet<AccountSession> Accounts { get; set; }
+        public DbSet<AccountSession> AccountSessions { get; set; }
+
         public CardStorageServiceDbContext(DbContextOptions options) : base(options) { }
 
     }
